a5b8da49a20c915d3bd79c1106058a5f98305486
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-04-18 21:52:56 +0600

    9 #ScreeShoter.py

---
+++ 9 #ScreeShoter.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+def take_screenshot(filename='screenshot.png'):
+    # Capture the screenshot
+    screenshot = pyautogui.screenshot()
+    
+    # Save the screenshot to a file
+    screenshot.save(filename)
+    
+    print(f"Screenshot saved as {filename}")
+
+# Example usage:
+take_screenshot()
